8a25329f9aba0990b833c9751c8241fbc6f76f64
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-27 12:26:59 +0100

    reindent routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,31 @@
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-      root to: 'pages#home'
-      devise_for :users
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  devise_for :users
 
-      resource :profile, only: [:show]
+  resource :profile, only: [:show]
 
-      resources :recipes, only: [:index, :show] do
-        resources :copies, only: [:create]
-      end
-
-      resources :contributions, only: [:index]
-
-      namespace :owner do
-        resource :dashboard, only: [:show]
-
-        resources :recipes, only: [:new, :create, :edit, :update, :destroy] do
-
-          resources :contributions, only: [:new, :create]
-          resources :steps, only: [:new, :create]
-        end
-        resources :steps, only: [:edit, :update, :destroy]
-        resources :changes_proposals, only: [:show] do
-          member do
-            patch :accept
-            patch :refuse
-          end
-        end
-      end
+  resources :recipes, only: [:index, :show] do
+    resources :copies, only: [:create]
   end
+
+  resources :contributions, only: [:index]
+
+  namespace :owner do
+    resource :dashboard, only: [:show]
+
+    resources :recipes, only: [:new, :create, :edit, :update, :destroy] do
+      resources :contributions, only: [:new, :create]
+      resources :steps, only: [:new, :create]
+    end
+
+    resources :steps, only: [:edit, :update, :destroy]
+
+    resources :changes_proposals, only: [:show] do
+      member do
+        patch :accept
+        patch :refuse
+      end
+    end
+  end
+end
