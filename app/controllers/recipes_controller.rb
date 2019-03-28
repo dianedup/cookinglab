d523809b13e0eba7389f5d26c245e12b144986d8
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-28 15:39:06 +0100

    index done

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,12 @@
+class RecipesController < ApplicationController
+  def index
+    if params[:query].present?
+      sql_query = " \
+              recipes.title @@ :query \
+            "
+      @recipes = Recipe.where("recipes.title @@ :query", query: "%#{params[:query].split(' ').join(' OR ')}%")
+    else
+      @recipes = Recipe.all
+    end
+  end
+end
