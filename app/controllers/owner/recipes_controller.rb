6a6924bc907ba96363360fcd946bcc8f20ae51e1
melanie rat <RAT.melanie@gmail.com>
2019-03-29 14:26:48 +0100

    fix text area

--- app/controllers/owner/recipes_controller.rb
+++ app/controllers/owner/recipes_controller.rb
@@ -5,11 +5,23 @@ class Owner::RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(params_recipe)
+    @recipe.user = current_user
     if @recipe.save
+      if params[:recipe][:tags].present? # => ["1", "2", "3"]
+        checked_tags = params[:recipe][:tags]
+        # pour chaque tag, créer la table de jointure recipe_tags en chopant l'objet Tag
+        checked_tags.each do |tag|
+          RecipeTag.create(recipe: @recipe, tag: Tag.find(tag))
+        end
+      end
       redirect_to edit_owner_recipe_path(@recipe)
     else
       render :new
     end
+  end
+
+  def edit
+    raise
   end
 
   private
