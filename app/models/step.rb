fc0b9aabd6d60713064d1bcf47e05601b2e2450a
Diane du Payrat <dianedupayrat@gmail.com>
2019-03-31 20:35:50 +0200

    Enable owner to edit recipe subtitle, times, description, add-edit-delete steps, add-edit-delete doses of ingredients

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,7 +1,7 @@
 class Step < ApplicationRecord
   belongs_to :recipe
-  has_many :step_utensils
+  has_many :step_utensils, dependent: :destroy
   has_many :utensils, through: :step_utensils
-  has_many :doses
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 end
