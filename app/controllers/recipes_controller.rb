acedc328d9aee8408438f8a544574de4e71234d0
melanie rat <RAT.melanie@gmail.com>
2019-03-31 19:47:13 +0200

    Add log out option to home page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,23 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
+
+  def index
+    if params[:query].present?
+      sql_query = " \
+              recipes.title @@ :query \
+            "
+      @recipes = Recipe.where(kind: "original").
+                        where("recipes.title @@ :query", query: "%#{params[:query].split(' ').join(' OR ')}%")
+    else
+      @recipes = Recipe.where(kind: "original")
+    end
+  end
   def show
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
