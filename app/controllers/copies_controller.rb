b291a1d6811fe9895a6722660ed9a8c8e3b44772
Diane du Payrat <dianedupayrat@gmail.com>
2019-04-02 16:14:14 +0200

    modify 'create variant service' into 'create copy service' to include changes proposal

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -1,6 +1,6 @@
 class CopiesController < ApplicationController
   def create
-    @new_variant = ::Recipes::CreateVariantService.new(params[:recipe_id], current_user.id).call
+    @new_variant = ::Recipes::CreateCopyService.new(params[:recipe_id], current_user.id).call
 
     redirect_to edit_owner_recipe_path(@new_variant)
   end
