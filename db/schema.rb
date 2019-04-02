e6a537fdaf75347ba8d50ddf992e2fcb7fec9fb3
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-04-02 10:33:41 +0200

    avatar thierry fixed and padding and margin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_04_01_140534) do
 
   # These are extensions that must be enabled in order to support this database
