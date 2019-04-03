8f1e38a72f46eddd7f9ea4b87f2e5fa2b3d91af5
Diane du Payrat <dianedupayrat@gmail.com>
2019-04-04 00:38:12 +0200

    add cp cards on dashboard, sorted by pending first and created_at :desc

--- app/controllers/owner/dashboards_controller.rb
+++ app/controllers/owner/dashboards_controller.rb
@@ -1,6 +1,16 @@
 class Owner::DashboardsController < ApplicationController
   def show
-    @current_user = current_user
     @recipes = current_user.recipes.order("created_at DESC")
+    @changes_proposals = []
+    @recipes.each do |recipe|
+      recipe.recipes_with_changes_proposals.each do |changes_proposal_recipe|
+        changes_proposal_recipe.changes_proposals.each do |changes_proposal| # only 1 cp by cp_recipe
+          @changes_proposals << changes_proposal
+        end
+      end
+    end
+    @changes_proposals.sort! { |cp| cp.created_at }
+    @changes_proposals.sort! { |cp1, cp2| cp1.status <=> cp2.status }
+    @changes_proposals.reverse!
   end
 end
