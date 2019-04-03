19b1b89c8fe1e3c6ae19c025800095a009f88ba9
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-04-03 12:40:13 +0200

    seeds updated

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,6 +8,7 @@ class Recipe < ApplicationRecord
   has_many :changes_proposals
   has_many :variants, -> { where(kind: 'variant') }, class_name: "Recipe", foreign_key: "original_recipe_id", dependent: :destroy
 
+
   mount_uploader :photo, PhotoUploader
 
   include PgSearch
