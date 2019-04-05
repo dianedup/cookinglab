861de1ad5930264acea15fe7f8d9aaf047fff9ff
Matthias Geoffroy <matthiasgeoffroy@gmail.com>
2019-04-05 11:50:55 +0200

    Fix select 2
    Signed-off-by: Matthias Geoffroy <matthiasgeoffroy@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import "bootstrap";
 import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
 initSelect2();
+window.initSelect2 = initSelect2;
 
 import { previewImageOnFileSelect } from "../components/photo_preview";
 previewImageOnFileSelect();
