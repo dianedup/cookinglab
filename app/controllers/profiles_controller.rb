2eeb9f4274862aaff031e6e0a3535d2d27b3e546
melanie rat <RAT.melanie@gmail.com>
2019-04-13 19:40:11 +0200

    add information on profile page and link to navbar

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,17 @@
+class ProfilesController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to profile_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
+end
