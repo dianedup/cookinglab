123338d98bdd64313e3f5ef76ef99a26e1421f90
Diane du Payrat <dianedupayrat@gmail.com>
2019-04-17 15:13:14 +0200

    Merge cp recipe details on original recipe when changes proposal accepted

--- app/controllers/owner/changes_proposals_controller.rb
+++ app/controllers/owner/changes_proposals_controller.rb
@@ -4,7 +4,9 @@ class Owner::ChangesProposalsController < ApplicationController
     @changes_proposal.status = 'accepted'
 
     if @changes_proposal.save
-      redirect_to owner_dashboard_path(anchor: "changes-proposals")
+      @updated_recipe = merge_cp_on_original_recipe
+      # redirect_to owner_dashboard_path(anchor: "changes-proposals")
+      redirect_to recipe_path(@updated_recipe)
     end
   end
 
@@ -15,5 +17,13 @@ class Owner::ChangesProposalsController < ApplicationController
     if @changes_proposal.save
       redirect_to owner_dashboard_path(anchor: "changes-proposals")
     end
+  end
+
+  private
+
+  def merge_cp_on_original_recipe
+    ::Recipes::MergeCpOnOriginalRecipeService.new(@changes_proposal.recipe.id).call
+    # recipe_id <=> id of recipe with kind = "changes_proposal", not original recipe
+
   end
 end
